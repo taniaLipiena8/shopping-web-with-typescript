ae68457e59e919e0fc62087aacbd8be00e859445
taniaLipiena8 <tanialipiena8@gmail.com>
2023-04-18 13:15:17 +0700

    fix cart

--- src/public-user/cart/cartExcerpt/CartExcerpt.tsx
+++ src/public-user/cart/cartExcerpt/CartExcerpt.tsx
@@ -10,26 +10,49 @@ interface CartExcerptProps {
   handleDecreased: (id: any) => void;
 }
 
-const CartExcerpt = ({cart, handleDelete, handleIncrease,handleDecreased,}: CartExcerptProps) => {
+const CartExcerpt = ({
+  cart,
+  handleDelete,
+  handleIncrease,
+  handleDecreased,
+}: CartExcerptProps) => {
   return (
     <div className="cartExcerpt">
       <Card className="cartCard">
         <Card.Img className="cart-image" src={cart.image} />
-        <Card.Body className="card-body">
-          <h5 className="card-title">{cart.title}</h5>
-          <p className="card-text">{cart.desc}</p>
-          <p className="card-text">${cart.price}</p>
+        <Card.Body className="cart-body">
+          <div>
+            <h5 className="card-title">{cart.title}</h5>
+            <p className="card-text">
+              {cart.desc.length <= 80
+                ? cart.desc
+                : `${cart.desc.slice(0, 80)}...`}
+            </p>
+            <p className="card-text">${cart.price}</p>
+          </div>
 
           <div className="cart-buttons">
-            <Button variant="danger" className="trash" onClick={() => handleDelete(cart.id)}>
+            <Button
+              variant="danger"
+              className="trash"
+              onClick={() => handleDelete(cart.id)}
+            >
               <FaTrash />
             </Button>
             <span>
-              <Button variant="primary" disabled={cart.quantity === 1 ? true : false} onClick={() => handleDecreased(cart.id)}>
+              <Button
+                variant="primary"
+                disabled={cart.quantity === 1 ? true : false}
+                onClick={() => handleDecreased(cart.id)}
+              >
                 -
               </Button>
               <span className="cart-amount">{cart.quantity}</span>
-              <Button variant="primary" disabled={cart.quantity === cart.stock ? true : false} onClick={() => handleIncrease(cart.id)}>
+              <Button
+                variant="primary"
+                disabled={cart.quantity === cart.stock ? true : false}
+                onClick={() => handleIncrease(cart.id)}
+              >
                 +
               </Button>
             </span>
